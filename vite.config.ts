06cea874d1d84df5159021c65752db9d81bf8501
SandPaper <nosandpaper@protonmail.com>
2023-11-29 19:38:20 +0100

    chore(config): Updated development environment config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
   },
   test: {
-    environment: "happy-dom",
-  }
-})
+    environment: 'happy-dom',
+    setupFiles: ['./setupVitest.js'],
+  },
+});
